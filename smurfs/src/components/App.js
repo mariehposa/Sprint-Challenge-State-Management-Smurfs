ee87052353be2959c536ff5006ea2b3a61ac8d83
Adedeji Mariam <maadedeji76@student.lautech.edu.ng>
2019-10-11 08:38:51 +0100

    use combineReducers

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+import thunk from 'redux-thunk';
+import { combineReducers, createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import * as reducers from './state/reducers';
+
+const monsterReducer = combineReducers({
+  smurf: reducers.smurfReducer,
+})
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        
+
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
